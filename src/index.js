52d5a384cc3d8f35b9aaad98674f0eec802dee77
fabio-pv <fabiopv1994@gmail.com>
2021-10-15 18:17:15 -0300

    clean

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import "moment/locale/pt-br";
 import Start from "./Start";
 
 ReactDOM.render(
@@ -9,7 +8,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a functionn
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
